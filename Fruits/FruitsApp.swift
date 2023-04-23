c735a418f40573ab2e255a654c18b8e4fbf7fa42
JimSweeneyJr <jimsweeney566@gmail.com>
2023-04-23 10:36:21 -0400

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by James Sweeney on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
